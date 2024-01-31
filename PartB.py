2cb106f2403bda44a2a7eb01e057f803da04efd8
MANNY-rt <B00143972@mytudublin.ie>
2024-01-31 12:09:33 +0000

    Part B created

---
+++ PartB.py
@@ -0,0 +1,19 @@
+#Emmanuel Aigbedion
+#B00143972
+#13/10/2003
+#Part B
+#The Program to print Steps 1 to 9 to the screen
+
+def printSteps(a, b):
+    for i in range(a, b+1):
+        print(f"Steps {i}")
+
+print("Step 1")
+print("Step 2")
+print("Step 3")
+
+printSteps(4,6)
+
+print("Step 7")
+print("Step 8")
+print("Step 9")
